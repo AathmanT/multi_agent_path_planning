c26472c5e8fda1fd3f133fef37d36fdff736c700
Aathman Tharmasanthiran <35717580+AathmanT@users.noreply.github.com>
2024-02-08 23:38:57 -0500

    === Working and not working Robot and wall radius

--- centralized/cbs/constants.py
+++ centralized/cbs/constants.py
@@ -1,10 +1,18 @@
 # for CBS collision checking
 
+# working configs
 ROBOT_MUlTIPLIER = 2
 OBSTACLE_MULTIPLIER = 2
 
 ROBOT_RADIUS = 0.1 * 4
 OBSTACLE_RADIUS = 0.1 * 4
+
+# # actual but not working configs
+# ROBOT_MUlTIPLIER = 4
+# OBSTACLE_MULTIPLIER = 3
+#
+# ROBOT_RADIUS = 4
+# OBSTACLE_RADIUS = 3
 
 # for visualization
 PLT_ROBOT_RADIUS = 0.5 * ROBOT_MUlTIPLIER  # 0.5 to plot the circle centered at the point, and 2 to increase the size of the circle to map to the actual robot size
